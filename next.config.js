4092c62f8bcdd8b64dc9bbb2222feac85975aabd
AYUSH SHETTY <ayushshetty@AYUSHs-MacBook-Air.local>
2026-08-25 14:34:34 +0530

    Fix: Full bin folder handling for chromium-min

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   outputFileTracingIncludes: {
     '/api/**/*': [
       'node_modules/@sparticuz/chromium-min/bin/**/*',
+      'node_modules/@sparticuz/chromium-min/bin',
       'node_modules/puppeteer-core/**/*',
     ],
   },
